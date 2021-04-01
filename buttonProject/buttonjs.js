164f300ddd143749bef35e8fc117124fb70299ec
ManavZP <goldengasher9@gmail.com>
2021-04-01 14:44:24 -0400

    button finis

--- buttonProject/buttonjs.js
+++ buttonProject/buttonjs.js
@@ -1,15 +1,36 @@
 var theBackground = document.getElementById("thebackground");
 
 var buttonMain = document.getElementById("mainButton");
-
+var numClicks = 0;
+var discoMusic = new Audio("disco.mp3");
 
 function disco(){
 
+    //color changes
     var randomColor = rgb(Math.random()*255, Math.random()*255, Math.random()*255);
     console.log(randomColor);
-
     theBackground.style.backgroundColor = randomColor;
 
+    numClicks++;
+
+    if(numClicks === 10){
+        discoMusic.play();
+        buttonMain.innerHTML = "DISCO TIME!!";
+        buttonMain.id = "newButton";
+        theBackground.id="newBackground";
+        setTimeout(stopMusic,25000);  
+    }
+
+
+    
+
+
+}
+function stopMusic() {
+    discoMusic.pause();
+    buttonMain.id = "mainButton";
+    theBackground.id="theBackground";
+    buttonMain.innerHTML = "What time is it?";
 
 }
 
